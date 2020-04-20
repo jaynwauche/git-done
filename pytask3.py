120372d0088f6e899dce7e9c7834ba992e655a00
Nwauche Junior Chimenka <juniornwauche@gmail.com>
2020-04-20 12:50:23 +0100

    added a function

--- pytask3.py
+++ pytask3.py
@@ -7,6 +7,8 @@ hard_number = random.randint(1, 50)
 guess_count = 0
 print(number)
 print('easy', easy_number)
+print('medium', medium_number)
+print('hard', hard_number)
 try:
     while True:
         user_guess_number = int(input('Guess a number: '))
@@ -17,37 +19,27 @@ try:
             print('Wrong answer')
 except ValueError:
     user_guess_number1 = (input('There are three levels: easy, medium, and hard, choose: '))
-if user_guess_number1 == 'easy':
-    for a in range(6):
-        num1 = int(input('Make a guess: '))
-        guess_count += 1
-        if num1 == easy_number:
-            print("You are right")
-            break
-        else:
-            print("That was wrong")
-    else:
-        print("Game over")
-if user_guess_number1 == 'medium':
-    for x in range(4):
-        num2 = int(input('Make a guess: '))
-        guess_count += 1
-        if num2 == medium_number:
-            print("You are right")
-            break
-        else:
-            print("That was wrong")
-    else:
-        print('Game over')
-elif user_guess_number1 == 'hard':
-    for b in range(3):
-        num1 = int(input('Make a guess: '))
-        guess_count += 1
-        if num1 == hard_number:
-            print("You are right")
-            break
-        else:
-            print("That was wrong")
-    else:
-        print('Game over')
+
+    # a function that will be called by the different levels of the game avoiding repeatation of code
+    def guessing(level_count, level_system_number, guess_count):
+        for a in range(level_count):
+            try:
+                num1 = int(input('Make a guess: '))
+                guess_count -= 1
+                if num1 == level_system_number:
+                    print("You are right!!!")
+                    break
+                else:
+                    print("That was wrong")
+                    print(f"You have {guess_count} guesses remaining")
+            except ValueError:
+                print('please enter a valid number')
+                
+    if user_guess_number1 == 'easy':
+        guessing(6, easy_number, 6)
+    elif user_guess_number1 == 'medium':
+        guessing(4, medium_number, 4)
+    elif user_guess_number1 == 'hard':
+        guessing(3, hard_number, 3)
+    
             
